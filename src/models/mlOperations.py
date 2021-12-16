6eeeebd63173e2a5686827c8342ebf182980f5a2
julianafalves <julianaferreira@estudante.ufscar.br>
2021-12-16 17:36:13 -0300

    add ML operations

---
+++ src/models/mlOperations.py
@@ -0,0 +1,34 @@
+import models.mlDispatcher as mlDispatcher
+from models.mlEvaluate import Evaluate
+import os, argparse, joblib
+import pandas as pd
+from sklearn import metrics
+
+
+class genericModel:
+    def __init__(self,algorithm) -> None:
+        """
+        Build ML model, using de parameters from mlParameters
+        """       
+        self.model = mlDispatcher.models[f"{algorithm}"]
+        self.name = algorithm
+        print(type(self.model))
+
+
+    def train(self,x_train, y_train):
+        """
+        Train ML model and save joblib in "AlzheimersDisease/models"
+        """
+        self.model.fit(x_train,y_train)
+        joblib.dump(self.model, f"models/trained_{self.name}.joblib")
+
+
+    def test(self, x_test, y_test):
+        """
+        Test ML model using metrics from mlEvaluate
+        """
+        y_predict = self.model.predict(x_test)
+        prob_predict = self.model.predict_proba(x_test)
+        Evaluate(y_test,y_predict,prob_predict)
+
+        return y_predict
